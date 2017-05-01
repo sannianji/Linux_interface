9a4e7245ea469a5152eede3750afa07d4a65bc99
sannian <sannianjisansan@gmail.com>
2017-05-01 14:58:16 +0800

    modify large_file

--- large_file.c
+++ large_file.c
@@ -1,21 +1,22 @@
 #define LARGEFILE64_SOURCE
+#define _FILE_OFFSET_BITS 64
 #include<sys/stat.h>
 #include<fcntl.h>
 #include<tlpi_hdr.h>
 int main(int argc,char * argv[])
 {
 	int fd;
-	off64_t off;
+	off_t off;
 	if(argc!=3||strcmp(argv[1],"--help")==0)
 		usageErr("%s pathname offset\n",argv[0]);
 	
-	fd=open64(argv[1],O_CREAT|O_RDWR,S_IRUSR|S_IWUSR);
+	fd=open(argv[1],O_CREAT|O_RDWR,S_IRUSR|S_IWUSR);
 	
 	if(fd==-1)
-		errEixt("open64");
+		errExit("open64");
 	
 	off=atoll(argv[2]);
-	if(lseek64(fd,off,SEEK_SET)==-1)
+	if(lseek(fd,off,SEEK_SET)==-1)
 		errExit("lseek64");
 	
 	if(write(fd,"test",4)==-1)
